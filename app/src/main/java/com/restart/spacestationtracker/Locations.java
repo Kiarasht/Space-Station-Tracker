6229b9167fcddd05fd25214b56fa955b4f4c1079
restart32 <kiarashbikar@gmail.com>
2015-11-25 17:50:55 -0800

    Finds passing time based on location

--- app/src/main/java/com/restart/spacestationtracker/Locations.java
+++ app/src/main/java/com/restart/spacestationtracker/Locations.java
@@ -11,20 +11,24 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Locations extends MapsActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = ".Locations";
-    private TextView textView;
+    private TextView countrycity;
+    private TextView isspasses;
     private String mLatitude;
     private String mLontitude;
     protected GoogleApiClient mGoogleApiClient;
@@ -33,7 +37,8 @@ public class Locations extends MapsActivity implements GoogleApiClient.Connectio
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_locations);
-        textView = (TextView) findViewById(R.id.textView2);
+        countrycity = (TextView) findViewById(R.id.textView2);
+        isspasses = (TextView) findViewById(R.id.textView3);
         buildGoogleApiClient();
     }
 
@@ -66,6 +71,7 @@ public class Locations extends MapsActivity implements GoogleApiClient.Connectio
             mLatitude = String.valueOf(mLastLocation.getLatitude());
             mLontitude = String.valueOf(mLastLocation.getLongitude());
             displayresults();
+            displaypasses();
         } else {
             Toast.makeText(this, "Unable to find your location.", Toast.LENGTH_LONG).show();
         }
@@ -81,7 +87,6 @@ public class Locations extends MapsActivity implements GoogleApiClient.Connectio
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
     }
-
 
     private void displayresults() {
         AsyncTask.execute(new Runnable() {
@@ -128,7 +133,73 @@ public class Locations extends MapsActivity implements GoogleApiClient.Connectio
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            textView.setText(mLocation);
+                            countrycity.setText(mLocation);
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void displaypasses() {
+        AsyncTask.execute(new Runnable() {
+            public void run() {
+                String strContent = "";
+
+                try {
+                    URL urlHandle = new URL("http://api.open-notify.org/iss-pass.json?lat=" + mLatitude + "&lon=" + mLontitude);
+                    URLConnection urlconnectionHandle = urlHandle.openConnection();
+                    InputStream inputstreamHandle = urlconnectionHandle.getInputStream();
+
+                    try {
+                        int intRead;
+                        byte[] byteBuffer = new byte[1024];
+
+                        do {
+                            intRead = inputstreamHandle.read(byteBuffer);
+
+                            if (intRead == 0) {
+                                break;
+
+                            } else if (intRead == -1) {
+                                break;
+                            }
+
+                            strContent += new String(byteBuffer, 0, intRead, "UTF-8");
+                        } while (true);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    inputstreamHandle.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    JSONArray aresults = new JSONObject(strContent).getJSONArray("response");
+                    int [] duration = new int[aresults.length()];
+                    Date [] date = new Date[aresults.length()];
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm", Locale.getDefault());
+                    final StringBuilder stringBuilder = new StringBuilder();
+
+                    for (int i = 0; i < aresults.length(); ++i) {
+                        JSONObject apass = aresults.getJSONObject(i);
+                        date[i] = new Date(Long.parseLong(apass.getString("risetime")) * 1000L);
+                        duration[i] = apass.getInt("duration") / 60;
+                        stringBuilder.append(i+1).append(".  ")
+                                .append(simpleDateFormat.format(date[i]))
+                                .append(" for ").append(duration[i])
+                                .append(" m\n\n");
+                    }
+
+                    Locations.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            isspasses.setText(stringBuilder);
                         }
                     });
 
