c4939cf8400f6f0d3a513e13ad4bd9cd38bea0b9
restart32 <kiarashbikar@gmail.com>
2016-06-21 03:55:21 -0700

    Adding one of the boxes.

--- app/src/main/java/com/restart/spacestationtracker/Preferences.java
+++ app/src/main/java/com/restart/spacestationtracker/Preferences.java
@@ -2,11 +2,21 @@ package com.restart.spacestationtracker;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class Preferences extends AppCompatActivity {
+    private static AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +24,12 @@ public class Preferences extends AppCompatActivity {
         setContentView(R.layout.layout_general);
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        adView = (AdView) findViewById(R.id.adView);
 
         SettingsFragment settingsFragment = new SettingsFragment();
         fragmentTransaction.add(android.R.id.content, settingsFragment, "SETTINGS_FRAGMENT");
         fragmentTransaction.commit();
-
+        loadPreferences();
     }
 
     public static class SettingsFragment extends PreferenceFragment {
@@ -28,6 +39,59 @@ public class Preferences extends AppCompatActivity {
 
             addPreferencesFromResource(R.xml.app_preferences);
             getPreferenceScreen().findPreference("refresh_Rate").setEnabled(true);
+
+            getPreferenceScreen().findPreference("advertisement").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    boolean checked = preference.getSharedPreferences().getBoolean("advertisement", false);
+                    Context context = getActivity().getApplicationContext();
+
+                    if (checked) {
+                        Toast.makeText(context, "Ads disabled. Consider enabling them when non-intrusive", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(context, "Ads enabled. Thanks for the support ;)", Toast.LENGTH_SHORT).show();
+                    }
+
+                    if (checked && adView != null) {
+                        adView.setVisibility(View.INVISIBLE);
+                    } else if (!checked) {
+                        if (adView == null) {
+                            AdRequest adRequest = new AdRequest.Builder().build();
+                            adView.loadAd(adRequest);
+                        } else {
+                            adView.setVisibility(View.VISIBLE);
+                        }
+                    }
+
+                    return true;
+                }
+            });
+
+            getPreferenceScreen().findPreference("notification_ISS").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Toast.makeText(getActivity().getApplicationContext(), preference.getSharedPreferences().getBoolean("notification_ISS", false) + "", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+
+            getPreferenceScreen().findPreference("notification_Astro").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Toast.makeText(getActivity().getApplicationContext(), preference.getSharedPreferences().getBoolean("notification_Astro", false) + "", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
         }
+    }
+
+    private void loadPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean advertisement = sharedPreferences.getBoolean("advertisement", false);
+
+        boolean notification_ISS = sharedPreferences.getBoolean("notification_ISS", false);
+        boolean notification_Astro = sharedPreferences.getBoolean("notification_Astro", false);
+        int refresh_Rate = sharedPreferences.getInt("refresh_Rate", 15);
     }
 }
