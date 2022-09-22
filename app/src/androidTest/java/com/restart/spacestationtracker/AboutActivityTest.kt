d12f502fc25b6375e026a82ea8b45ea4fe59a9e7
Kiarash <kiarash.torkian@nike.com>
2022-09-21 18:21:25 -0700

    Creating CI/CD for this project

--- app/src/androidTest/java/com/restart/spacestationtracker/AboutActivityTest.kt
+++ app/src/androidTest/java/com/restart/spacestationtracker/AboutActivityTest.kt
@@ -1,22 +1,21 @@
 package com.restart.spacestationtracker
 
+import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.activityScenarioRule
-import androidx.test.runner.AndroidJUnit4
-import org.junit.Rule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MapsActivityTest {
-    @get:Rule
-    val rule = activityScenarioRule<MapsActivity>()
+class AboutActivityTest {
 
     @Test
-    fun testMapsActivityActionBar() {
-        onView(withId(R.id.title_text)).check(matches(withText(R.string.map_activity)))
+    fun testEvent() {
+        launchActivity<AboutActivity>().use {
+            onView(withId(R.id.about_title)).check(matches(withText(R.string.app_name)))
+        }
     }
 }
